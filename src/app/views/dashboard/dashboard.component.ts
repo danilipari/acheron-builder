40b94ee61b56cb86715dc2b35da1e0cc426dba25
Dani Lipari <lipari.assistenza@gmail.com>
2022-08-23 11:17:13 +0200

    updates dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -6,15 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  countResurces: any = [
-    { "workflows": localStorage.getItem('_local_db_workflows') ? localStorage.getItem('_local_db_workflows') : 0 },
-    { "forms": localStorage.getItem('_local_db_forms') ? localStorage.getItem('_local_db_forms') : 0 },
-    { "layouts": localStorage.getItem('_local_db_layouts') ? localStorage.getItem('_local_db_layouts') : 0 },
-  ];
+  countResurces: any = []
 
   constructor() { }
 
   ngOnInit() {
+    this.countResurces = [
+      { "workflows": localStorage.getItem('_local_db_workflows') ? localStorage.getItem('_local_db_workflows') : 0 },
+      { "forms": localStorage.getItem('_local_db_forms') ? localStorage.getItem('_local_db_forms') : 0 },
+      { "layouts": localStorage.getItem('_local_db_layouts') ? localStorage.getItem('_local_db_layouts') : 0 },
+    ];
+
     this.countResurces = this.countResurces.map((element: string) => (
       { [Object.keys(element)[0]]: JSON.parse(Object.values(element)[0]) }
     ));
