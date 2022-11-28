dd01157a117694447d1bf1b28a9935fd9ebf947c
Dani Lipari <lipari.assistenza@gmail.com>
2022-11-28 16:39:15 +0100

    update labels

--- src/app/services/strapi-babylon2.service.ts
+++ src/app/services/strapi-babylon2.service.ts
@@ -50,6 +50,4 @@ export class StrapiBabylon2Service {
     return this.http.get(`${API}content-manager/collection-types/application::${section}.${section}?page=${page}&pageSize=${size}&_sort=${sortKey}:${sortOrder}`, httpOptionsBearer);
   }
 
-  // content-manager/collection-types/application::label.label?page=1&pageSize=10&_sort=label_title:ASC
-
 }
