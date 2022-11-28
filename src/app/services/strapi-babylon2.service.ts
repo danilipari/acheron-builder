6e5439f8635bf4b1505ebdd8b5f6734424eaf37e
Dani Lipari <lipari.assistenza@gmail.com>
2022-11-28 16:33:15 +0100

    update countries

--- src/app/services/strapi-babylon2.service.ts
+++ src/app/services/strapi-babylon2.service.ts
@@ -46,14 +46,6 @@ export class StrapiBabylon2Service {
     return this.http.get(`${API}content-manager/content-types`, httpOptionsBearer);
   }
 
-  getCategoriesItems(page: string | number, size: string | number) {
-    return this.http.get(`${API}content-manager/collection-types/application::category.category?page=${page}&pageSize=${size}&_sort=category_title:ASC`, httpOptionsBearer);
-  }
-
-  getCountriesItems(page: string | number, size: string | number) {
-    return this.http.get(`${API}content-manager/collection-types/application::country.country?page=${page}&pageSize=${size}&_sort=Country:ASC`, httpOptionsBearer);
-  }
-
   getTableCollectionItems(section: string, page: string | number, size: string | number, sortKey: string, sortOrder: string = "ASC") {
     return this.http.get(`${API}content-manager/collection-types/application::${section}.${section}?page=${page}&pageSize=${size}&_sort=${sortKey}:${sortOrder}`, httpOptionsBearer);
   }
